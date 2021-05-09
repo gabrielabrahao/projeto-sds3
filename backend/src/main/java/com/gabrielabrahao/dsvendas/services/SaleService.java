4fe3eab166ab8dac86aa3d0ca1d5f7371b9f802b
gabrielabrahao <gabriel.abrahao95@gmail.com>
2021-05-08 22:44:22 -0300

    Pagination

--- backend/src/main/java/com/gabrielabrahao/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/gabrielabrahao/dsvendas/services/SaleService.java
@@ -1,5 +1,22 @@
 package com.gabrielabrahao.dsvendas.services;
 
-public class SaleService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+import com.gabrielabrahao.dsvendas.dto.SaleDTO;
+import com.gabrielabrahao.dsvendas.entities.Sale;
+import com.gabrielabrahao.dsvendas.repositories.SaleRepossitory;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepossitory repossitory;
+
+	public Page<SaleDTO> findAll(Pageable pageable){
+		Page<Sale> result =  repossitory.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
 }
--- backend/src/main/java/com/gabrielabrahao/dsvendas/services/SellerService.java
+++ backend/src/main/java/com/gabrielabrahao/dsvendas/services/SellerService.java
@@ -5,9 +5,11 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gabrielabrahao.dsvendas.dto.SellerDTO;
 import com.gabrielabrahao.dsvendas.entities.Seller;
+import com.gabrielabrahao.dsvendas.repositories.SaleRepossitory;
 import com.gabrielabrahao.dsvendas.repositories.SellerRepossitory;
 
 @Service
@@ -17,7 +19,12 @@ public class SellerService {
 	@Autowired
 	private SellerRepossitory repossitory;
 	
+	@Autowired
+	private SaleRepossitory saleRepossitory;
+	
+	@Transactional(readOnly = true)
 	public List<SellerDTO> findAll(){
+		saleRepossitory.findAll();
 		List<Seller> result =  repossitory.findAll();
 		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
 	}
